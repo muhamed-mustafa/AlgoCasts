c05a8dd1d9339ba12c6822646c8730af937fa953
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2022-07-27 18:19:28 +0200

    solved capitalize and steps problems

---
+++ exercises/steps/index.js
@@ -0,0 +1,62 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n , row = 0 , stair = '')
+{
+    // second soultion
+    if(n === row)
+    {
+        return;
+    }
+
+    if(n === stair.length)
+    {
+        console.log(stair);
+        return steps(n , row + 1);
+    }
+
+    const add = stair.length <= row ? '#' : ' ';
+    steps(n , row , stair + add);
+};
+
+/*
+function steps(n) {
+    first soultion :
+      for(let row = 0; row < n; row++)
+      {
+          let stair ='';
+          for(let column = 0; column < n; column++)
+          {
+            if(column <= row)
+            {
+                stair += '#';
+            }
+
+            else
+            {
+                stair += ' ';
+            }
+          }
+        
+        console.log(stair);
+      }
+  }
+*/
+
+module.exports = steps;
