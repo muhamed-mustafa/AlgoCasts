800606290099dac286888f4496046519fbb596e0
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2022-08-03 10:58:35 +0200

    solved fib problem

---
+++ exercises/fib/index.js
@@ -0,0 +1,57 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+function memoize(fn)
+{
+    const cache = {};
+    return function(...args)
+    {
+        if(cache[args])
+        {
+            return cache[args];
+        }
+
+        const result = fn.apply(this,args);
+        cache[args] = result;
+        return result;
+    }
+}
+
+// second soultion by recursive 
+function slowFib(n)
+{
+    if(n < 2)
+    {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
+
+/*
+first solution :
+  function fib(n) {
+    let result = [0,1];
+
+    for(let i = 2; i <= n; i++)
+    {
+        a = result[i - 2];
+        b = result[i - 1];
+
+        result.push(a + b);
+    }
+
+    return result[n];
+  }
+*/
+
+module.exports = fib;
