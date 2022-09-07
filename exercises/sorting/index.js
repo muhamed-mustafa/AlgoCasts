4913db802268088343b013b1311cab695b71bd97
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2022-09-07 14:03:40 +0200

    Implement insertionSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,80 +1,81 @@
 // --- Directions
-// Implement bubbleSort, selectionSort, and mergeSort
+// Implement bubbleSort, selectionSort, and mergeSort, InsertionSort
 
 function bubbleSort(arr) {
-  
-  for(let i = 0; i < arr.length; i++)
-  {
-      for(let j = 0; j < (arr.length - i - 1); j++)
-      {
-          if(arr[j] > arr[j + 1])
-          {
-              const lesser = arr[j + 1];
-              arr[j + 1] = arr[j];
-              arr[j] = lesser;
-          }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
       }
+    }
   }
 
   return arr;
 }
 
 function selectionSort(arr) {
-
-    for(let i = 0; i < arr.length; i++)
-    {
-        let indexOfMin = i;
-        for(let j = i + 1; j < arr.length; j++)
-        {
-            if(arr[j] < arr[indexOfMin])
-            {
-                indexOfMin = j;
-            }
-        }
-
-        if(indexOfMin !== i)
-        {
-            let lesser = arr[indexOfMin];
-            arr[indexOfMin] = arr[i];
-            arr[i] = lesser
-        }
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
     }
 
-    return arr;
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+
+  return arr;
 }
 
 function mergeSort(arr) {
-    if(arr.length === 1)
-    {
-        return arr;
-    }
+  if (arr.length === 1) {
+    return arr;
+  }
 
-    const center = Math.floor(arr.length / 2);
-    const left   = arr.slice(0 , center);
-    const right  = arr.slice(center);
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
 
-
-    return merge(mergeSort(left), mergeSort(right));
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  const results = [];
 
-    const results = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
 
-    while(left.length && right.length)
-    {
-        if(left[0] < right[0])
-        {
-            results.push(left.shift());
-        }
+  return [...results, ...left, ...right];
+}
 
-        else
-        {
-            results.push(right.shift());
-        }
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j;
+
+    for (j = i - 1; j >= 0 && arr[j] > current; j--) {
+      arr[j + 1] = arr[j];
     }
 
-    return [...results , ...left , ...right];
+    arr[j + 1] = current;
+  }
+
+  return arr;
 }
+
+console.log(insertionSort([5, 3, 6, 1, 4])); // [1, 3, 4, 5, 6]
+console.log(insertionSort([11, 15, 12, 10, -2])); // [-2, 10, 11, 12, 15]
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
